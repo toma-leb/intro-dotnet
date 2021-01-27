4448297b446152fe1856eda02eaba161336462cb
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2021-01-27 09:54:41 +0100

    Create ASP.NET MVC application

--- src/DataProvider/Person.cs
+++ src/DataProvider/Person.cs
@@ -2,5 +2,19 @@
 
 namespace DataProvider
 {
-    public record Person (Guid PersonId, string FirstName, string LastName, int Age);
+    public class Person
+    {
+        public Person(Guid personId, string firstName, string lastName, int age)
+        {
+            PersonId = personId;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public Guid PersonId { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public int Age { get; }
+    }
 }
