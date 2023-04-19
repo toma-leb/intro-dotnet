9cac356efff7425b640aca1303a5fd60c0cd4b67
Matthias Grosperrin <matthias.g@mozzaik365.com>
2023-04-19 12:51:05 +0200

    Initial version of WPF app

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DataProvider;
+using System.Windows;
+
+namespace WpfApp1;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = new MainWindowsModel(new PersonRepository());
+    }
+}
---
+++ WpfApp1/MainWindowsModel.cs
@@ -0,0 +1,78 @@
+﻿using DataProvider;
+using Microsoft.Xaml.Behaviors.Core;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public class MainWindowsModel : INotifyPropertyChanged
+    {
+        private readonly IPersonRepository _personRepository;
+        private PersonViewModel? _currentPerson;
+        private ObservableCollection<Person> people;
+
+        public MainWindowsModel(IPersonRepository personRepository)
+        {
+            _personRepository = personRepository;
+            LoadPeople();
+            SelectPersonCommand = new ActionCommand(person => SelectPerson((Person)person));
+            SaveCommand = new ActionCommand(Save);
+        }
+
+        private void LoadPeople()
+        {
+            People = new ObservableCollection<Person>(_personRepository.GetAll());
+
+        }
+
+        public ObservableCollection<Person> People
+        {
+            get => people; set
+            {
+                people = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(People)));
+            }
+        }
+        public ICommand SelectPersonCommand { get; set; }
+        public ICommand SaveCommand { get; set; }
+        public PersonViewModel? CurrentPerson
+        {
+            get => _currentPerson; set
+            {
+                _currentPerson = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentPerson)));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void SelectPerson(Person person)
+        {
+            if(person == null) {
+                CurrentPerson = null;
+                return;
+            }
+            CurrentPerson = new PersonViewModel
+            {
+                Id = person.PersonId,
+                FirstName = person.FirstName,
+                LastName = person.LastName,
+                Age = person.Age
+            };
+        }
+        public void Save()
+        {
+            _personRepository.Update(CurrentPerson.Id, CurrentPerson.FirstName, CurrentPerson.LastName, CurrentPerson.Age);
+            LoadPeople();
+        }
+    }
+    public class PersonViewModel
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
