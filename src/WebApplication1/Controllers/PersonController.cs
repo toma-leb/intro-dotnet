841b729dcb70525550f097bb5ce4a08f371c82fd
Thomas Leblond <thomas.leblond@epita.fr>
2023-04-20 11:52:05 +0200

    done

---
+++ src/WebApplication1/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataProvider;
+
+namespace WebApplication1.Controllers
+{
+    public class PersonController : Controller
+    {
+        public PersonRepository persRepository = new PersonRepository();
+        public ActionResult Index()
+        {
+            List<Person> persList = persRepository.GetAll().ToList();
+            ViewBag.NbColumns = 4;
+            return View(persList);
+        }
+    }
+}
