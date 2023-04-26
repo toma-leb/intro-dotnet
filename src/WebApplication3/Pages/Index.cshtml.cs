2b3d5fb14c48e822c6cfa63cb5d23173647f856c
Thomas Leblond <thomas.leblond@epita.fr>
2023-04-26 12:24:38 +0200

    add minimum

--- src/WebApplication3/Pages/Index.cshtml.cs
+++ src/WebApplication3/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataProvider;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
@@ -19,7 +20,6 @@ namespace WebApplication3.Pages
 
         public void OnGet()
         {
-
         }
     }
 }
--- src/WebApplication3/Startup.cs
+++ src/WebApplication3/Startup.cs
@@ -1,3 +1,4 @@
+using DataProvider;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +24,7 @@ namespace WebApplication3
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<IPersonRepository, PersonRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
